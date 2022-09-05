4c11a46ed212fa37a2a01bc98eac2e8b868f04b9
José María Gutiérrez <josemariagutierrez@u2204wsl2jmgr.dir.svc.accenture.com>
2022-09-05 16:07:50 +0200

    A wrapper attempt.

--- vars/isDevelopBranch.groovy
+++ vars/isDevelopBranch.groovy
@@ -1,9 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String branch) {
-    return "develop".equals(branch)
-}
-
-def call(String branch, Boolean wrapper = true) {
-    return "develop".equals(branch)
+def invokingGlobalLibrary(String branch) {
+    return isDevelopBranch(branch)
 }
