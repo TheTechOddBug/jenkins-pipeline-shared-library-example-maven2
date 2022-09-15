6fe3d5f1b30da99e4a88f63bd5593021022b52aa
José María Gutiérrez <josemariagutierrez@u2204wsl2jmgr.dir.svc.accenture.com>
2022-09-15 09:24:40 +0200

    Testing conflicts

--- vars/isDevelopBranch.groovy
+++ vars/isDevelopBranch.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String branch) {
+def call(String branch, String param5, String param6, String param7 = "test", String param8 = "test", String param9 = "test") {
     // return "develop".equals(branch)
     return "==>==>Invocación desde libreria maven2<==<=="
 }
